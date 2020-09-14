3f1622417e668d06b34292bcf79fddbb8a6be409
Luscas03 <58983721+Luscas03@users.noreply.github.com>
2020-09-14 20:59:50 -0300

    aula08

--- ionic-api/src/app/app-routing.module.ts
+++ ionic-api/src/app/app-routing.module.ts
@@ -2,48 +2,49 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  //rota da página inicial da aplicação
+  // Rota da página inicial da aplicação
   {
     path: '',
     redirectTo: 'inicio',
     pathMatch: 'full'
   },
- 
- //Rota da página home
+
+  // Rota da página "inicio"
   {
     path: 'inicio',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
-  //error 404
+
+  // Rota para a página "e404"
   {
     path: 'e404',
     loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
   },
+
+  // Rota para a página "sobre"
   {
     path: 'sobre',
     loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
   },
-  {
-    path: 'contato',
-    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
-  },
-  {
-    path: 'forum',
-    loadChildren: () => import('./pages/forum/forum.module').then( m => m.ForumPageModule)
-  },
-  
+
+  // Rota para listar todos os usuários
   {
     path: 'usuarios',
     loadChildren: () => import('./users/list/list.module').then( m => m.ListPageModule)
   },
-  //error para rotas inexistentes OBS: TEM QUE SER SEMPRE A ULTIMA.
+
+  // Rota para exibir usuário único
+  {
+    path: 'usuario/:id',
+    loadChildren: () => import('./users/user/user.module').then( m => m.UserPageModule)
+  },
+
+  // Rota para rotas inexistentes - DEVE SER SEMPRE A ÚLTIMA ROTA
   {
     path: '**',
     loadChildren: () => import('./pages/e404/e404.module').then( m => m.E404PageModule)
   }
-  
-  
- 
+
 ];
 
 @NgModule({
--- ionic-api/src/app/services/users.service.ts
+++ ionic-api/src/app/services/users.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
 
-//Importar as bibliotecas assincronas do Angular 
+// Importar as bilbiotecas assíncronas do Angular
 import { Observable } from 'rxjs';
 
-//importar bibliotecas HTTP
+// Importar as bilbiotecas HTTP
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-private apiurl = 'http://localhost:8888/api';
- 
-constructor(
+
+  // URL "base" da API
+  private apiurl = 'http://localhost:8888/api';
+
+  constructor(
     private http: HttpClient
   ) { }
-//Método para obter todos os usuarios da API
-getUsers(): Observable<any> {
-  //faz o get da api
-  return this.http.get(this.apiurl);
-}
 
+  // Método para obter todos os usuários da API
+  getUsers(): Observable<any> {
+
+    // Faz o GET na API
+    return this.http.get(this.apiurl);
+  }
+
+  // Método para listar um usuário específico pelo Id
+  getUser(id: string): Observable<any> {
+
+    // Consulta à API no formato "http://localhost:8888/api?id={id}"
+    return this.http.get(`${this.apiurl}?id=${id}`);
+  }
 }
